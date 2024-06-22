4cf2e90ee29d7a51fa87aa15027b9bc66105cfab
AllaHanol <156720109+AllaHanol@users.noreply.github.com>
2024-06-22 15:05:21 +0200

    localstorage

--- js/localstorage.js
+++ js/localstorage.js
@@ -1,37 +1,14 @@
-'use strict';
-import { saveToLS, loadFromLS } from './helper.js';
-
-const formData = {
-  email: '',
-  message: '',
-};
-
-  const form = document.querySelector('.feedback-form');
-  const emailInputElem = form.querySelector("input[name='email']");
-  const messageInputElem = form.querySelector("textarea[name = 'message']");
-  const storageKey = 'feedback-form-state';
-  const savedFormData = loadFromLS(storageKey);
-  
-  if (savedFormData) {
-    formData.email = savedFormData.email;
-    formData.message = savedFormData.message;
-  }
-  
-  emailInputElem.value = formData.email;
-  messageInputElem.value = formData.message;
-  
-  form.addEventListener('input', e => {
-    formData[e.target.name] = e.target.value.trim();
-    saveToLS(storageKey, formData);
-  });
-  
-  form.addEventListener('submit', e => {
-    e.preventDefault();
-    if (emailInputElem.value && messageInputElem.value) {
-      console.log(formData);
-      localStorage.removeItem(storageKey);
-      form.reset();
-    } else {
-      alert('Fill please all fields');
+export function saveToLS(key,value){
+    const jsonData = JSON.stringify(value);
+    localStorage.setItem(key, jsonData);
+}
+export function loadFromLS(key){
+    const json =localStorage.getItem(key);
+    try{
+        const data = JSON.parse (json);
+        return data;
     }
-  });
+    catch{
+        return json;
+    }
+}
