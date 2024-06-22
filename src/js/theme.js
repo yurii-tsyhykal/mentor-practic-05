3861d317f168258ba36775c3f869756ac05adafc
Yurii <y.tsyhykal@gmail.com>
2024-06-22 16:04:48 +0200

    theme-func

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,3 +1,8 @@
 import { refs } from './refs';
 
-
+export function initTheme() {
+  const checkTheme = loadFromLS('theme');
+  if (checkTheme === 'dark') {
+    refs.bodyEl.classList.toggle('dark-theme');
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { onClickChangeTheme } from './js/hendlers';
 import { refs } from './js/refs';
+import { initTheme } from './js/theme';
+initTheme();
 
 refs.switchEl.addEventListener('click', onClickChangeTheme);
