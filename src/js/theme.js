b754360bdbbecac4e9782017e94bbe9884972ad1
Yurii <y.tsyhykal@gmail.com>
2024-06-26 16:58:34 +0200

    add ptrn v-hidden for checkbox

--- src/js/theme.js
+++ src/js/theme.js
@@ -6,8 +6,8 @@ export function onLoadedDomTheme() {
   const checkTheme = loadFromLS(key) ?? 'light';
   saveToLS(key, checkTheme);
   if (checkTheme === 'dark') {
-    refs.bodyEl.classList.toggle('dark-theme');
-    refs.modal.classList.toggle('dark-theme');
+    refs.bodyEl.classList.add('dark-theme');
+    refs.modal.classList.add('dark-theme');
     refs.theme.innerHTML = `
       <svg class="moon" height="30" width="30">
         <use href="/img/icons.svg#moon"></use>
